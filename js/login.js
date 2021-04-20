3902c79420ead37a3226343316d06d0e69cb2927
anmolsharma4545 <anmol.sharma4545@gmail.com>
2021-04-20 23:36:48 +0530

    init

---
+++ js/login.js
@@ -0,0 +1,21 @@
+const sign_in_btn = document.querySelector('#sign-in-btn');
+const sign_up_btn = document.querySelector('#sign-up-btn');
+const container = document.querySelector('.container');
+const redirect_btn = document.querySelector('#redirect');
+const redirected_btn = document.querySelector('#redirected');
+sign_up_btn.addEventListener('click', () => {
+    container.classList.add("sign-up-mode");
+    
+});
+
+sign_in_btn.addEventListener('click', () => {
+    container.classList.remove("sign-up-mode");
+});
+
+redirect_btn.addEventListener('click', () => {
+	window.open('user.htm');
+});
+
+redirected_btn.addEventListener('click', () => {
+	window.open('user.htm');
+});
---
+++ js/main.js
@@ -0,0 +1,6 @@
+const register = document.querySelector('#register_btn');
+
+register.addEventListener('click' , () => {
+    alert('You have successfully Registered With Us..');
+    window.open('./login.htm');
+});
